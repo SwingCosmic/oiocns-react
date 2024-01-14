f247a19f371469faf6616181e4c4d0a313a06694
15168347908 <296778233@qq.com>
2024-01-14 22:54:41 +0800

    fix: 判空

--- src/ts/core/work/executor/webhook.ts
+++ src/ts/core/work/executor/webhook.ts
@@ -8,11 +8,13 @@ import orgCtrl from '@/ts/controller';
 export class Webhook extends Executor {
   async execute(): Promise<boolean> {
     this.changeProgress(0);
+    const work = await this.task.findWorkById(this.task.taskdata.defineId);
     await kernel.httpForward({
       uri: this.metadata.hookUrl,
       method: 'POST',
       header: { 'Content-Type': 'application/json' },
       content: JSON.stringify({
+        work: work?.metadata,
         taskData: this.task.taskdata,
         person: orgCtrl.user.metadata,
         belong: this.task.belong.metadata,
